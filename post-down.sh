8f5bcb2b9bc49004cf01dd9f809eda390d83dae9
Stefan Steinmüller <stefan@steinmueller-me.com>
2026-02-22 13:22:29 +0100

    chore: add some more to the scrip

--- post-down.sh
+++ post-down.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
+interface=$1
+event=$2
+
+if [[ $interface != "wlan0" ]] || [[ $event != "down" ]]
+then
+  return 0
+fi
+
 sh -c "/usr/bin/killall /usr/sbin/parprouted"
 sh -c "/sbin/ifdown eth0"
--- post-up.sh
+++ post-up.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
+
+interface=$1
+event=$2
+
+if [[ $interface != "wlan0" ]] || [[ $event != "up" ]]
+then
+  return 0
+fi
 sh -c "/usr/sbin/parprouted eth0 wlan0"
   # clone the dhcp-allocated IP to eth0 so dhcp-helper will relay for the correct subnet
 sh -c "/sbin/ip addr add $(/sbin/ip addr show wlan0 | perl -wne 'm|^\s+inet (.*)/| && print $1')/32 dev eth0"
